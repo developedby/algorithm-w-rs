c40433193e8aa30ef7ecbb381a1a3cece8e43d93
Nicolas Abril <nicolas.abril@protonmail.ch>
2024-08-12 22:25:23 +0200

    Initial implementation

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+/* CLI */
+
+use algorithm_w::{infer, Program, ProgramTypes};
+
+fn main() {
+  match do_cli() {
+    Ok(types) => {
+      println!("{types}");
+    }
+    Err(e) => {
+      eprintln!("{e}");
+    }
+  }
+}
+
+fn do_cli() -> Result<ProgramTypes, String> {
+  // First argument is the file to parse.
+  let mut args = std::env::args();
+  args.next();
+  let file = args.next().expect("missing file argument");
+
+  // Read the file.
+  let contents = std::fs::read_to_string(file).expect("failed to read file");
+
+  // Parse the file.
+  let program = contents.parse::<Program>()?;
+
+  // Infer the types.
+  let types = infer(program)?;
+  Ok(types)
+}
